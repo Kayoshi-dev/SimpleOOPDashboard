3a0895db6d1cbe7ffc6a8e3b75011c1609cdc3dd
Kayoshi-dev <maxime.jme@gmail.com>
2019-07-21 15:55:07 +0200

    Singleton class Database

--- libraries/Database.php
+++ libraries/Database.php
@@ -3,16 +3,20 @@
 
 class Database
 {
+    private static $instance = null;
+
     public static function getPdo(): PDO {
-        $data = parse_ini_file('keys.ini');
-        try {
-            $dsn = 'mysql:dbname=' . $data['dbname'] . ';host=' . $data['host'];
-            $user = $data['user'];
-            $password = $data['password'];
-            $pdo = new PDO($dsn, $user, $password);
-        } catch (PDOException $exception) {
-            echo $exception->getMessage();
+        if(self::$instance === null) {
+            $data = parse_ini_file('keys.ini');
+            try {
+                $dsn = 'mysql:dbname=' . $data['dbname'] . ';host=' . $data['host'];
+                $user = $data['user'];
+                $password = $data['password'];
+                self::$instance = new PDO($dsn, $user, $password);
+            } catch (PDOException $exception) {
+                echo $exception->getMessage();
+            }
         }
-        return $pdo;
+        return self::$instance;
     }
 }
